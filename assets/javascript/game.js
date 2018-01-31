56a9cf439ab270ea9662437078b91b18a5d09594
Jason M. Aron <jasonaronart@gmail.com>
2018-01-31 14:52:26 -0500

    Hangman-Game-2.0

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,127 @@
 
-    hint.onclick = function() {
 
-      hints = [
-        [""]
-    ];
 
-    var catagoryIndex = categories.indexOf(chosenCategory);
-    var hintIndex = chosenCategory.indexOf(word);
-    showClue.innerHTML = "Clue: - " +  hints [catagoryIndex][hintIndex];
-  };
 
-   // Reset
 
- document.getElementById('reset').onclick = function() {
-    correct.parentNode.removeChild(correct);
-    letters.parentNode.removeChild(letters);
-    showClue.innerHTML = "";
-    context.clearRect(0, 0, 400, 400);
-    play();
-  }
+
+
+// Data variables
+var wordBank = ['Peacock', 'Giraffe', 'Polar Bear', 'Arctic Fox', 'Blue Whale', 'Tiger', 'Sea Otter', 'Snow Leopard', 'Gorillas'];
+
+
+var winCount = 0;   
+var lettersGuessed = [];                   
+var livesLeft = 10;
+var hangmanWord = "";
+var underscoreWord = [];
+
+
+
+
+//Game logic
+function setHangmanWord() {
+	hangmanWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+}
+
+function newUnderscoreWord() {
+
+	for (var i = 0 ; i < hangmanWord.length; i++) {
+		underscoreWord.push("_")
+	}
+}
+
+function replaceUnderScore(guess) {
+	for (var i = 0; i < hangmanword.length; i++)
+		if (guess === hangmanWord[i]) {
+			underscoreWord[i] = guess;
+		}
+	}
+
+
+	function decreaseLivesLeft() {
+		livesLeft--;
+	}
+
+	function increasewWinCount() {
+		winCount++;
+	}
+
+	function checkLives() {
+		if (livedLeft === 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	function pushToLettersGuessed(guess) {
+		lettersGuessed.push(guess);
+	}
+
+
+	document.addEventListener('keyup', event => {
+		var userGuess = event.key.toUpperCase();
+		manageGamePlay(userGues);
+	});
+
+	function isGuessCorrect(guess) {
+		if (hangmanWord.indexOf(guess) !== -1) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	var alphabet = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+
+
+	function manageGamePlay(guess) {
+
+		if (alphabet.indexOf(guess) !== -1) {
+
+	//screen letters already guessed
+	if (lettersGuessed.indexOf(guess) === -1 && 
+		underscoreWord.indexOf(guess) === -1) {
+
+
+		if(isGuessCorrect(guess)) {
+			replaceUnderScore(guess);
+
+		} else {
+			decreaseLivesLeft();
+			pushToLettersGuessed();
+		}
+	}
+
+
+
+
+	
+	// * Step 1: Recied Guess
+
+
+	// * Step 2: Screen Guess:
+	// *					  * screen anything that's not a letter
+	// *					  * Screen out previously guessed letter
+
+	// *Step 2b: Evaluate guess: is user correct or incorrect 
+	// 					*if correct replace underscore with letter
+	// 					*if incorrect decreaseLivesLeft(), pushLettersGuessed
+
+	// * Step 3: If letter is correct
+	// * Step 3b: If letter Guess is incorrect
+
+
+	// *Step 4: If they die: function call to aller, reset, 
+	// *
+
+
+
+
+
+
+
+
 
 
 
