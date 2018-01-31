bf1b350e8aec4d30fbacdbfcfc7f6e2c42b334c3
Jason M. Aron <jasonaronart@gmail.com>
2018-01-30 22:25:44 -0500

    Hangman-Game-1.5

---
+++ assets/javascript/game.js
@@ -0,0 +1,24 @@
+
+    hint.onclick = function() {
+
+      hints = [
+        [""]
+    ];
+
+    var catagoryIndex = categories.indexOf(chosenCategory);
+    var hintIndex = chosenCategory.indexOf(word);
+    showClue.innerHTML = "Clue: - " +  hints [catagoryIndex][hintIndex];
+  };
+
+   // Reset
+
+ document.getElementById('reset').onclick = function() {
+    correct.parentNode.removeChild(correct);
+    letters.parentNode.removeChild(letters);
+    showClue.innerHTML = "";
+    context.clearRect(0, 0, 400, 400);
+    play();
+  }
+
+
+
