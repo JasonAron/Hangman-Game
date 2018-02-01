40aba847e2171698750b7f4d6360faa3cdc183a5
Jason M. Aron <jasonaronart@gmail.com>
2018-02-01 17:19:32 -0500

    Hangman-Game-2.5

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,35 +5,41 @@
 
 
 
-// Data variables
-var wordBank = ['Peacock', 'Giraffe', 'Polar Bear', 'Arctic Fox', 'Blue Whale', 'Tiger', 'Sea Otter', 'Snow Leopard', 'Gorillas'];
+	// Data variables
+	var wordBank = ['Peacock', 'Giraffe', 'Polar Bear', 'Arctic Fox', 'Blue Whale', 'Tiger', 'Sea Otter', 'Snow Leopard', 'Gorillas'];
 
 
-var winCount = 0;   
-var lettersGuessed = [];                   
-var livesLeft = 10;
-var hangmanWord = "";
-var underscoreWord = [];
+	var winCount = 0;   
+	var lettersGuessed = [];                   
+	var livesLeft = 10;
+	var hangmanWord = "";
+	var underscoreWord = [];
+	var alphabet = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+
+	var userGuessDiv = document.getElementById("userGuess");
+
+	//Game logic
 
 
 
 
-//Game logic
-function setHangmanWord() {
-	hangmanWord = wordBank[Math.floor(Math.random() * wordBank.length)];
-}
-
-function newUnderscoreWord() {
-
-	for (var i = 0 ; i < hangmanWord.length; i++) {
-		underscoreWord.push("_")
+	function setHangmanWord() {
+		hangmanWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+		console.log(hangmanWord)
 	}
-}
 
-function replaceUnderScore(guess) {
-	for (var i = 0; i < hangmanword.length; i++)
-		if (guess === hangmanWord[i]) {
-			underscoreWord[i] = guess;
+
+	function newUnderscoreWord() {
+		for (var i = 0 ; i < hangmanWord.length; i++) {
+			underscoreWord.push("_")
+		}
+	}
+
+
+	function replaceUnderScore(guess) {
+		for (var i = 0; i < hangmanword.length; i++)
+			if (guess === hangmanWord[i]) {
+				underscoreWord[i] = guess;
 		}
 	}
 
@@ -56,47 +62,56 @@ function replaceUnderScore(guess) {
 
 	function pushToLettersGuessed(guess) {
 		lettersGuessed.push(guess);
-	}
+		}
+
 
 
 	document.addEventListener('keyup', event => {
 		var userGuess = event.key.toUpperCase();
-		manageGamePlay(userGues);
+			manageGamePlay(userGuess);
 	});
 
 	function isGuessCorrect(guess) {
-		if (hangmanWord.indexOf(guess) !== -1) {
-			return true;
-		} else {
-			return false;
-		}
+	if (hangmanWord.indexOf(guess) !== -1) {
+		return true;
+	} else {
+		return false;
+	}
 	}
 
-	var alphabet = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
 
 
 	function manageGamePlay(guess) {
 
-		if (alphabet.indexOf(guess) !== -1) {
+	if (alphabet.indexOf(guess) !== -1) {
 
 	//screen letters already guessed
 	if (lettersGuessed.indexOf(guess) === -1 && 
-		underscoreWord.indexOf(guess) === -1) {
+	underscoreWord.indexOf(guess) === -1) {
 
 
-		if(isGuessCorrect(guess)) {
-			replaceUnderScore(guess);
+	if(isGuessCorrect(guess)) {
+		replaceUnderScore(guess);
+		// var userGuessDiv.innerHTML() = newUnderscoreWord; 
+
+			
 
 		} else {
-			decreaseLivesLeft();
-			pushToLettersGuessed();
+		decreaseLivesLeft();
+		pushToLettersGuessed();
 		}
 	}
 
 
-
+		} else {
+	// do nothing
+		}
+	}
 
 	
+
+
+
 	// * Step 1: Recied Guess
 
 
